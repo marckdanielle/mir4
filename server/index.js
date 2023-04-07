483628fb275b0e8a8270ba0df1e688ff92721af7
danielleconquilla <marck.conquilla@gmail.com>
2023-04-07 21:16:17 +0800

    fixing res.sendFile

--- server/index.js
+++ server/index.js
@@ -20,8 +20,7 @@ const buildPath = path.join(_dirname, '../client/build');
 app.use(express.static(buildPath))
 
 app.get("/*", function(req, res) {
-  res.sendFile(
-    path.join(__dirname, "../client/build/index.html"),
+  res.sendFile('index.html', { root: __dirname },
     function(err) {
       if(err) {
         res.status(500).send(err);
